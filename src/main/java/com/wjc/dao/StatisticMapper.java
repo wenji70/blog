689d4cc06a4aeb088c8d197a8e7846d88d7a8a74
wenji70 <2256996256@qq.com>
2025-12-20 10:51:35 +0800

    邮件定时发送版

--- src/main/java/com/wjc/dao/StatisticMapper.java
+++ src/main/java/com/wjc/dao/StatisticMapper.java
@@ -32,5 +32,12 @@ public interface StatisticMapper {
     @Select("SELECT * FROM t_statistic WHERE hits !='0' " +
             "ORDER BY hits DESC, comments_num DESC")
     public List<Statistic> getStatistic();
+    // 统计博客文章总访问量
+    @Select("SELECT SUM(hits) FROM t_statistic")
+    public long getTotalVisit();
+
+    // 统计博客文章总评论量
+    @Select("SELECT SUM(comments_num) FROM t_statistic")
+    public long getTotalComment();
 
 }
