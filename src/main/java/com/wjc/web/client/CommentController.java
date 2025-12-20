ce1e473f69fb66743c3f914a4e72d34cc28faf20
wenji70 <2256996256@qq.com>
2025-12-20 19:05:21 +0800

    dangao

--- src/main/java/com/wjc/web/client/CommentController.java
+++ src/main/java/com/wjc/web/client/CommentController.java
@@ -1,22 +1,17 @@
 package com.wjc.web.client;
-
 import com.github.pagehelper.PageInfo;
 import com.vdurmont.emoji.EmojiParser;
 import com.wjc.model.ResponseData.ArticleResponseData;
 import com.wjc.model.domain.Comment;
-import com.wjc.model.domain.User;
 import com.wjc.service.ICommentService;
-
 import com.wjc.utils.MyUtils;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.Map;
@@ -28,21 +23,22 @@ public class CommentController {
     private static final Logger logger =
             LoggerFactory.getLogger(CommentController.class);
     @Autowired
-    private ICommentService commentService;
+    private ICommentService commentServcie;
 
     // 发表评论操作
-    @PostMapping(value = "/comments/publish")
+    @PostMapping(value = "/publish")
     @ResponseBody
-    public ArticleResponseData publishComment(HttpServletRequest request, @RequestParam
-    Integer aid, @RequestParam String text) {
-// 去除js脚本
+    public ArticleResponseData publishComment(HttpServletRequest request,
+                                              @RequestParam Integer aid,
+                                              @RequestParam String text) {
+        // 去除js脚本
         text = MyUtils.cleanXSS(text);
         text = EmojiParser.parseToAliases(text);
-// 获取当前登录用户
+        // 获取当前登录用户
         org.springframework.security.core.userdetails.User securityUser =
                 (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String username = securityUser.getUsername();
-// 封装评论信息
+        // 封装评论信息
         Comment comments = new Comment();
         comments.setArticleId(aid);
         comments.setIp(request.getRemoteAddr());
@@ -50,27 +46,30 @@ public class CommentController {
         comments.setAuthor(username);
         comments.setContent(text);
         try {
-            commentService.pushComment(comments);
-            logger.info("发布评论成功，对应文章id: {}", aid);
+            commentServcie.pushComment(comments);
+            logger.info("发布评论成功，对应文章id: " + aid);
             return ArticleResponseData.ok();
         } catch (Exception e) {
-            logger.error("发布评论失败，对应文章id: {};错误描述: {}", aid, e.getMessage());
+            logger.error("发布评论失败，对应文章id: " + aid + ";错误描述: " + e.getMessage());
             return ArticleResponseData.fail();
         }
     }
 
-    @Operation(summary = "查询评论")
-    @PostMapping("/comments/getByid")
-    public String getCommentById(@RequestParam Integer aid, @RequestParam Integer page, @RequestParam Integer count) {
-        PageInfo<Comment> comments = commentService.getComments(aid, page, count);
-        return comments.toString();
+    //查询评论
+    @Operation(summary = "分页查询评论")
+    @PostMapping("/comments/getAll")
+    public String getCommentById(HttpServletRequest request, @RequestParam Integer aid,
+                                 @RequestParam Integer page, @RequestParam Integer count) {
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        PageInfo<Comment> comments = commentServcie.getComments(aid, page, count);
+        return "comm/getAllcomments";
     }
 
     //删除评论
     @Operation(summary = "删除评论")
-    @PostMapping("/comments/delete")
-    public String deleteCommentById(@RequestParam Integer id) {
-        commentService.deleteCommentWithId(id);
+    @PostMapping("/delete")
+    public String deleteCommentById(HttpServletRequest request, @RequestParam Integer id) {
+        commentServcie.deleteCommentWithId(id);
         return "删除成功";
     }
     //更新评论
